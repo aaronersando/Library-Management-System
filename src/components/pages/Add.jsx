5d425eeaed9ff9d1a01e684ce2a49a71d95c59fd
aaronersando <agersando@gmail.com>
2025-06-15 17:17:57 +0800

    Initial commit. React, tailwind, and firebase setup done

---
+++ src/components/pages/Add.jsx
@@ -0,0 +1,10 @@
+
+function Add(){
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Add;
---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXPgik5xv81cqzaN2JxLRe8Cw0ZcFnNy8",
+  authDomain: "library-management-syste-d71c5.firebaseapp.com",
+  projectId: "library-management-syste-d71c5",
+  storageBucket: "library-management-syste-d71c5.firebasestorage.app",
+  messagingSenderId: "956710199282",
+  appId: "1:956710199282:web:11448c7e549dbf1e7a956b",
+  measurementId: "G-Q0LXWFD16M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
