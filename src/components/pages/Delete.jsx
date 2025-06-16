02e3565c410074bebd574eeda7de2ab38d13f1b1
aaronersando <agersando@gmail.com>
2025-06-16 08:02:01 +0800

    basic delete functionality done

--- src/components/pages/Delete.jsx
+++ src/components/pages/Delete.jsx
@@ -1,8 +1,23 @@
+import {doc, deleteDoc} from "firebase/firestore"
+import { db } from "../../config/firebase"
 
 function Delete(){
 
+    const deleteBook = async (id)=> {
+        try{
+            await deleteDoc(doc(db, "books", id));
+            console.log("Book deleted with Id: ", id)
+        }catch(err){
+            console.error(err)
+        }
+        
+    }  
+
+
+
     return(
         <>
+            <button onClick={() => deleteBook(book.id)}>Delete Movie</button>
         </>
     )
 }
